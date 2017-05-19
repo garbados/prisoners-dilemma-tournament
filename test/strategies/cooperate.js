55f137c2aa958a0ec54ab0422cbd8e3bd2e41be8
Diana Thayer <garbados@gmail.com>
2017-05-18 21:33:57 -0700

    new tests

---
+++ test/strategies/cooperate.js
@@ -0,0 +1,24 @@
+/* global describe, it */
+
+var assert = require('assert')
+var strategy = require('../../lib/strategies/cooperate')
+
+const HISTORY = [
+  [true, false],
+  [false, true],
+  [true, true]
+]
+
+describe('strategies.cooperate', function () {
+  it('should process histories into choices', function () {
+    HISTORY.forEach(function (round, i) {
+      // test the strategy against successive subsets of the history
+      var historySubset = HISTORY.slice(0, i)
+      var choice = strategy(historySubset)
+      // assert that it returns a boolean
+      assert([true, false].indexOf(choice) > -1)
+    })
+  })
+
+  // write additional tests as appropriate
+})
---
+++ test/strategies/drone.js
@@ -0,0 +1,24 @@
+/* global describe, it */
+
+var assert = require('assert')
+var strategy = require('../../lib/strategies/drone')
+
+const HISTORY = [
+  [true, false],
+  [false, true],
+  [true, true]
+]
+
+describe('strategies.drone', function () {
+  it('should process histories into choices', function () {
+    HISTORY.forEach(function (round, i) {
+      // test the strategy against successive subsets of the history
+      var historySubset = HISTORY.slice(0, i)
+      var choice = strategy(historySubset)
+      // assert that it returns a boolean
+      assert([true, false].indexOf(choice) > -1)
+    })
+  })
+
+  // write additional tests as appropriate
+})
