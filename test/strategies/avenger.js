54da0b59ae44bbf20c0b3ad953f1fd64b749b4bf
Diana Thayer <garbados@gmail.com>
2017-05-18 23:24:21 -0700

    update avenger for coverage n bugs

--- test/strategies/avenger.js
+++ test/strategies/avenger.js
@@ -5,19 +5,25 @@ var strategy = require('../../lib/strategies/avenger')
 
 const HISTORY = [
   [true, false],
+  [false, false],
+  [false, false],
+  [false, false],
+  [false, false],
   [false, true],
-  [true, true]
+  [false, true],
+  [false, true]
 ]
 
 describe('strategies.avenger', function () {
   it('should process histories into choices', function () {
-    HISTORY.forEach(function (round, i) {
-      // test the strategy against successive subsets of the history
-      var historySubset = HISTORY.slice(0, i)
-      var choice = strategy(historySubset)
-      // assert that it returns a boolean
-      assert([true, false].indexOf(choice) > -1)
-    })
+    var historySubset
+    var choice
+    for (var i = 0; i < HISTORY.length; i++) {
+      historySubset = HISTORY.slice(0, i)
+      choice = strategy(historySubset)
+      console.log(choice, i, HISTORY[i][0])
+      assert(choice === HISTORY[i][0])
+    }
   })
 
   // write additional tests as appropriate
